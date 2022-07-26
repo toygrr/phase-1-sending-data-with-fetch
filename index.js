90771771d6d861f104645aedaaa1f8d8500ff49b
Tyler <toygrrr@gmail.com>
2022-07-26 13:37:12 -0700

    commit tree 11d0d329265d6355d58a3af83c6b4b533d92b05f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name, //why did i have to seperate these lines?
+      email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((obj) => (document.body.innerHTML = obj["id"])) // i dont understand this part
+    .catch((error) => (document.body.innerHTML = error.message)); //why am I setting the text of the HTML? and not appending to the json.db file?
+}
